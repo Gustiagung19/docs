a9c3b58faca1255c9d68257cbaf8dde9e04ee727
Gusti Agung <75777108+Gustiagung19@users.noreply.github.com>
2024-11-22 20:33:37 +0800

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+    logo: <span>My Project</span>,
+    project: {
+        link: 'https://github.com/Gustiagung19/aklgaming',
+    },
+    chat: {
+        link: 'https://discord.com',
+    },
+    docsRepositoryBase: 'https://github.com/Gustiagung19/aklgaming/blob/master',
+}
+
+export default config
